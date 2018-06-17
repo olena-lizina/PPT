6b6428d2b486ad52eef16b6ee8e0530039b8e913
Elena Lizina <elena.lizina@comodo.com>
2018-06-17 20:18:08 +0300

    Added GradesInfoModel

---
+++ cpp/GradesInfoModel.h
@@ -0,0 +1,71 @@
+/****************************************************************************
+**
+** Copyright (C) 2017-2018 Olena Lizina
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+** http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+**
+****************************************************************************/
+#ifndef REPORTINFOMODEL_H
+#define REPORTINFOMODEL_H
+#include <QObject>
+#include <QString>
+#include "DataTypes.h"
+
+class ReportInfoModel : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int id READ id)
+    Q_PROPERTY(QString studName READ studName WRITE setStudName NOTIFY studNameChanged)
+    Q_PROPERTY(QString finished READ finished WRITE setFinished NOTIFY finishedChanged)
+    Q_PROPERTY(QString mark     READ mark     WRITE setMark     NOTIFY markChanged    )
+    Q_PROPERTY(QString evalDate READ evalDate WRITE setEvalDate NOTIFY evalDateChanged)
+
+public:
+    ReportInfoModel(const QString studName, const Report rep)
+        : QObject(nullptr)
+        , mId(rep.id)
+        , mStudName(studName)
+        , mFinishDate(rep.delivDate)
+        , mMark(rep.mark)
+        , mEvalDate(rep.evalDate)
+    {}
+
+public slots:    
+
+    int id() const { return mId; }
+    QString studName() const { return mStudName; }
+    QString finished() const { return mFinishDate; }
+    QString mark() const { return mMark; }
+    QString evalDate() const { return mEvalDate; }
+
+    void setStudName(QString name) { mStudName = name; }
+    void setFinished(QString finish) { mFinishDate = finish; }
+    void setMark(QString mark) { mMark = mark; }
+    void setEvalDate(QString eval) { mEvalDate = eval; }
+
+signals:
+
+    void studNameChanged();
+    void finishedChanged();
+    void markChanged    ();
+    void evalDateChanged();
+
+private:
+    const int mId;
+    QString mStudName;
+    QString mFinishDate;
+    QString mMark;
+    QString mEvalDate;
+};
+#endif // REPORTINFOMODEL_H
